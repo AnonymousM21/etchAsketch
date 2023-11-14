23370cbbf028bd0a237b987c4f8e0c31c170f690
Melaku N <mnegussie403@gmail.com>
2023-11-14 22:08:42 +0300

    Iteration 3: Add a button to the top of the screen
    added a button to the top of the screen that will send
    the user a popup asking for the number of squares.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 const container = document.querySelector(".container");
+const changeGrid = document.querySelector(".changeGrid");
 
 container.style = "width: 256px; height: 256px";
 
-for (let i = 0; i < 16; i++) {
-  for (let row = 0; row < 16; row++) {
-    const grids = document.createElement("div");
-    grids.style = "width: 16px; height: 16px;";
-    grids.className = "grids";
-    grids.style = "width: 16px; height: 16px;";
-    container.appendChild(grids);
+drawGrids();
+changeGrid.addEventListener("click", customGrid);
+
+function drawGrids(noOfSquares = 16, width = 256 / noOfSquares) {
+  for (let i = 0; i < noOfSquares; i++) {
+    for (let row = 0; row < noOfSquares; row++) {
+      const grids = document.createElement("div");
+      grids.className = "grids";
+      grids.style = `width: ${width}px; height: ${width}px;`;
+      container.appendChild(grids);
+    }
   }
+}
+
+function customGrid() {
+  let squares, newWidth;
+
+  do {
+    squares = +prompt("Enter custom grid size between 1-100 inclusively.");
+  } while (squares < 1 || squares > 100);
+
+  newWidth = 256 / squares;
+  container.textContent = "";
+  drawGrids(squares, newWidth);
 }
