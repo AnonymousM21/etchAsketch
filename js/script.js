b760a19f2d318450719fa20cb86d06ec9575fb8a
Melaku N <mnegussie403@gmail.com>
2023-11-14 21:56:45 +0300

    Iteration 2: Set up a “hover” effect and some code improvement
    set up a “hover” effect so that the grid divs change color when
    a mouse passes over them, leaving a (pixelated) trail through
    the grid like a pen would.
    
    some css styles moved to css/style.css to not mix css with js.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ container.style = "width: 256px; height: 256px";
 for (let i = 0; i < 16; i++) {
   for (let row = 0; row < 16; row++) {
     const grids = document.createElement("div");
-    grids.style = "width: 16px; height: 16px; display: inline-block";
+    grids.style = "width: 16px; height: 16px;";
+    grids.className = "grids";
+    grids.style = "width: 16px; height: 16px;";
     container.appendChild(grids);
   }
 }
