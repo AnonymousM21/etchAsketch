19d054dcb8bc748d3a5ed0ecb5e4ffe8b79255e1
Melaku N <mnegussie403@gmail.com>
2023-11-15 10:01:37 +0300

    Fix a bug on iteration 2 commit[b760a19] & Rename variable names
    set up a “hover” effect so that the grid divs change color when a  mouse
    passes over them, leaving a (pixelated) trail through the grid like a
    pen would.
    
    some confusing variables names got renamed to avoid confusion.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+let pixelSize;
 const container = document.querySelector(".container");
 const changeGrid = document.querySelector(".changeGrid");
 
@@ -5,26 +6,38 @@ container.style = "width: 256px; height: 256px";
 
 drawGrids();
 changeGrid.addEventListener("click", customGrid);
+container.addEventListener("mouseenter", paint);
 
-function drawGrids(noOfSquares = 16, width = 256 / noOfSquares) {
+function drawGrids(noOfSquares = 16, widthAndHeight = 256 / noOfSquares) {
+  pixelSize = widthAndHeight;
   for (let i = 0; i < noOfSquares; i++) {
     for (let row = 0; row < noOfSquares; row++) {
       const grids = document.createElement("div");
       grids.className = "grids";
-      grids.style = `width: ${width}px; height: ${width}px;`;
+      grids.style = `width: ${widthAndHeight}px; height: ${widthAndHeight}px;`;
       container.appendChild(grids);
     }
   }
 }
 
 function customGrid() {
-  let squares, newWidth;
+  let squares, newWidthAndHeight;
 
   do {
     squares = +prompt("Enter custom grid size between 1-100 inclusively.");
   } while (squares < 1 || squares > 100);
 
-  newWidth = 256 / squares;
+  newWidthAndHeight = 256 / squares;
   container.textContent = "";
-  drawGrids(squares, newWidth);
+  drawGrids(squares, newWidthAndHeight);
+}
+
+function paint() {
+  const pixels = document.querySelectorAll(".container div");
+
+  pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseleave", () => {
+      pixel.style = `width: ${pixelSize}px; height: ${pixelSize}px; background-color: aqua;`;
+    });
+  });
 }
